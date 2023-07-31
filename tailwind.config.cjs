74c377fcdade5050cc5a5b6e58010dfb4b755144
j-hazart <jessy.hazart@gmail.com>
2023-07-31 21:39:27 +0200

    add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary:"#f2f6fa",
+				secondary:"#3e3750",
+				tertiary:"#ff7e33",
+				"purple-light":"#2a233e",
+				"purple-dark":"#171424",
+			},
+		},
 	},
 	plugins: [],
 }
