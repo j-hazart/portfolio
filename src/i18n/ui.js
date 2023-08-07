c152b029b77ccb0781713a2ecda7260c73c9ba96
j-hazart <jessy.hazart@gmail.com>
2023-08-07 09:31:33 +0200

    add change language option

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -1,6 +1,6 @@
 export const languages = {
-  en: 'English',
   fr: 'Français',
+  en: 'English',
 };
 
 export const defaultLang = 'fr';
@@ -9,11 +9,11 @@ export const ui = {
   en: {
     'nav.about': {path:'about', title: 'about me'},
     'nav.projects': {path:'projects', title: 'projects'},
-    'screenshot': 'screenshot',
   },
   fr: {
     'nav.about': {path:'à propos', title: 'à propos'},
     'nav.projects': {path:'projets', title: 'projets'},
-    'screenshot': "capture d'écran",
   },
 };
+/* ui[lang] */
+console.log(Object.entries(ui["en"]));
--- src/i18n/utils.js
+++ src/i18n/utils.js
@@ -1,4 +1,4 @@
-import { ui, defaultLang } from './ui';
+import { ui, defaultLang, languages } from './ui';
 
 export function getLangFromUrl(url) {
   const [, lang] = url.pathname.split('/');
@@ -11,3 +11,20 @@ export function useTranslations(lang) {
     return ui[lang][key] || ui[defaultLang][key];
   }
 }
+
+export function changeCurrentPathLang(path,from,to) {
+  const pathnamelayers = path.split("/");
+  const [, , page, ...rest] = pathnamelayers;
+  const decodedPageURL = decodeURIComponent(page);
+
+  const currentLangEntries = Object.entries(ui[from]);
+  const foundedEntry = currentLangEntries.find(([, val]) =>
+    val.path.includes(decodedPageURL)
+  );
+
+  const key = foundedEntry ? foundedEntry[0] : null;
+  return page
+  ? `/${to}/${foundedEntry ? ui[to][key].path : decodedPageURL}${rest && `/${rest.join("/")}`}`
+  : `/${to}`;
+}
+
