31787ee8bfc1263a8e9bab91a3507a11af17a2f1
j-hazart <jessy.hazart@gmail.com>
2023-08-21 23:53:48 +0200

    add component for home avatar & add alt translate

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -7,11 +7,34 @@ export const defaultLang = 'fr';
 
 export const ui = {
   en: {
-    'nav.about': {path:'about', title: 'about me'},
-    'nav.projects': {path:'projects', title: 'projects'},
+    "nav.about": { path: "about", title: "about me" },
+    "nav.projects": { path: "projects", title: "projects" },
   },
   fr: {
-    'nav.about': {path:'à propos', title: 'à propos'},
-    'nav.projects': {path:'projets', title: 'projets'},
+    "nav.about": { path: "à propos", title: "à propos" },
+    "nav.projects": { path: "projets", title: "projets" },
+  },
+};
+
+export const alt = {
+  en: {
+    "avatar": "Jessy Hazart's photo",
+    "thumbnail": "thumbnail",
+    "burger": "burger button",
+    "translate": "translate logo",
+    "screenshot": "screenshot",
+    "left.arrow": "left arrow",
+    "right.arrow": "right arrow",
+    "button.close": "close button",
+  },
+  fr: {
+    "avatar": "Photo de Jessy Hazart",
+    "thumbnail": "miniature",
+    "burger": "bouton burger",
+    "translate": "logo traduction",
+    "screenshot": "capture d'écran",
+    "left.arrow": "flèche gauche",
+    "right.arrow": "flèche droite",
+    "button.close": "bouton fermer",
   },
 };
