3c289e514409821fbdb7b3090fbf09852afbfd26
j-hazart <jessy.hazart@gmail.com>
2023-08-24 00:06:07 +0200

    refactoring about page code

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -31,6 +31,17 @@ export const alt = {
     activity: "web developer",
     "meta.home":
       "Explore the portfolio of Jessy Hazart, a dedicated web developer. Discover my projects, skills, and experiences. Connect with me on LinkedIn, explore my projects on GitHub, and check out my CV to learn more.",
+    "meta.about":
+      "Learn more about me through a brief introduction and the key skills that fuel my passion in the field of web development.",
+    "meta.projects":
+      "Explore my projects gallery and discover a diverse collection of creations in the technologies I master.",
+    "meta.contact":
+      "I am at your service to address your inquiries, discuss potential collaborations, or engage in simple exchanges. Your contact is highly anticipated and will be handled with the utmost professionalism.",
+    hello: "hello",
+    "about.presentation":
+      "Passionate about the web and new technologies since a young age, I have specialized in web development to create innovative and intuitive digital solutions.",
+    "about.cv": "My resume",
+    "about.pageTitle": "About me",
   },
   fr: {
     avatar: "Photo de Jessy Hazart",
@@ -46,5 +57,16 @@ export const alt = {
     activity: "développeur web",
     "meta.home":
       "Découvrez le portfolio de Jessy Hazart, développeur web passionné. Explorez mes projets, compétences et expériences. Connectez-vous avec moi sur LinkedIn, explorez mes projets sur GitHub et consultez mon CV pour en savoir plus.",
+    "meta.about":
+      "Découvrez-en plus sur moi à travers une courte présentation et les compétences clés qui alimentent ma passion dans le domaine du développement web.",
+    "meta.projects":
+      "Explorez ma galerie de projets et découvrez une collection diversifiée de réalisations dans les technologies que je maîtrise.",
+    "meta.contact":
+      "Je suis à votre disposition pour répondre à vos questions, discuter de collaborations potentielles ou échanger tout simplement des salutations. Votre prise de contact est vivement attendue et sera traitée avec le plus grand professionnalisme.",
+    hello: "bonjour",
+    "about.presentation":
+      "Passionné par l'univers du web et des nouvelles technologies depuis mon plus jeune âge, je me suis spécialisé dans le développement web afin de créer des solutions numériques innovantes et intuitives.",
+    "about.cv": "Consultez mon CV",
+    "about.pageTitle": "À propos de moi",
   },
 };
